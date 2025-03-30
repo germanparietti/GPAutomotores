09ffac1372e42d35027927d82a2ebd63f3195404
germanparietti <germanparietti@gmail.com>
2025-03-30 11:54:40 -0300

    login refact, falta implementar borrar input antes de registrase

--- src/test/loginTest.ts
+++ src/test/loginTest.ts
@@ -1,12 +1,19 @@
+import 'dotenv/config'; // <- Carga las variables del archivo .env
 import puppeteer from 'puppeteer';
 import { loginFacebook } from '../services/loginFacebook';
 
 (async () => {
-  const browser = await puppeteer.launch({ headless: false, args: ['--start-maximized'] });
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ['--start-maximized'],
+    timeout: 60000,
+  });
+
   const page = await browser.newPage();
   await page.setViewport({ width: 1366, height: 768 });
 
   await loginFacebook(page);
 
+  console.log('✅ Test finalizado. Cerrando navegador...');
   await browser.close();
 })();
